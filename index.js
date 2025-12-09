85c5becbc5178ec5a266b6653783808fa118278a
Resalat Al Samin <risalat731@gmail.com>
2025-12-10 01:34:13 +0600

    mongoDb connected

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const port = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// Mongodb Connection string
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.plgjzw8.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("eTuitionBD_db");
+    const tuitionsCollection = db.collection("tuitions");
+
+    // Api Fetching starts here
+    // User Api
+    app.get("/tuitions", async (req, res) => {});
+
+    app.post("/tuitions", async (req, res) => {
+      const tuition = req.body;
+      const result = await tuitionsCollection.insertOne(tuition);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("eTuitionBd is running fine");
